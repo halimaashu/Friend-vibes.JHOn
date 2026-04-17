856d6c843e0412bf2a84af14303fee9dd5e56e71
md ashik mia <halima520ashu@gmail.com>
2026-04-17 13:05:03 +0600

    ipliment sort function features

--- src/app/timeline/page.jsx
+++ src/app/timeline/page.jsx
@@ -2,12 +2,20 @@
 
 import { Context } from "@/provider/ContextProvider";
 import Image from "next/image";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 
 const page = () => {
   const [remember, setRemember] = useContext(Context);
-  //   console.log(remember);
-
+    console.log(remember);
+const [sortType,setSortType]=useState("")
+const haldleSort=()=>{
+const sorted = [...remember].sort((a, b) =>
+    a.type.toLowerCase().localeCompare(b.type.toLowerCase())
+  );
+  setRemember(sorted);
+  // console.log(sortArray,"dort array")
+}
+console.log(sortType)
   return (
     <div className=" space-y-10 ">
       {remember.length === 0 ? (
@@ -19,6 +27,21 @@ const page = () => {
         </div>
       ) : (
         <div className="py-20 px-5 bg-base-100 space-y-10">
+          <h1 className="text-2xl font-bold">Time LIne</h1>
+          <div className="dropdown ">
+            <div tabIndex={0} role="button" className="btn m-1">
+              Sort ⬇️
+            </div>
+            <ul
+              tabIndex="-1"
+              className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
+            >
+              <li>
+                <a onClick={haldleSort}>Sort By Title</a>
+              </li>
+              
+            </ul>
+          </div>
           {remember.map((log, index) => (
             <div
               key={index}
